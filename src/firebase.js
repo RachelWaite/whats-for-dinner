dd8a0ab85bf892bffb3a3fdeba7d408099924850
Rachel Waite <rachelwaite88@outlook.com>
2026-03-11 13:11:57 +1100

    Move secrets to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeDmZpKp6rp2gZ0icquHrbxyOmNB6qfeU",
-  authDomain: "whats-for-dinner-ab2c4.firebaseapp.com",
-  projectId: "whats-for-dinner-ab2c4",
-  storageBucket: "whats-for-dinner-ab2c4.firebasestorage.app",
-  messagingSenderId: "848202993957",
-  appId: "1:848202993957:web:a8d42fe2253b96d6bfcacc"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
